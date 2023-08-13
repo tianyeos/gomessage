5a3b808b81951cd6b9bf2c2c0a4867d763f983bb
古寒飞 <tay3223@qq.com>
2023-08-13 21:15:53 +0800

    新增钉钉客户端艾特人的功能6，逻辑链路改造完成

--- pkg/services/core/v3/dingtalk.go
+++ pkg/services/core/v3/dingtalk.go
@@ -1,6 +1,7 @@
 package v3
 
 import (
+	"encoding/json"
 	"gomessage/pkg/models"
 	"gomessage/pkg/services/core/v1"
 	"gomessage/pkg/services/format"
@@ -17,18 +18,23 @@ func (c *ClientActionDingtalk) RendersMessages(client *models.Client, isMerge bo
 	//参考文档：https://open.dingtalk.com/document/robots/custom-robot-access
 	//参考文档2：https://m.dingtalk.com/qidian/help-detail-1060904202
 
-	var mobiles []string = []string{"18516187075", "13071003191"} //手机号
-	var dingtalkIds []string = []string{"rpn519f"}                //钉钉号（注意：不是userID，而是钉钉号，一定要注意区分）
+	//var mobiles []string = []string{"18516187075", "13071003191"} //手机号
+	//var dingtalkIds []string = []string{"rpn519f"} //钉钉号（注意：不是userID，而是钉钉号，一定要注意区分）
+
+	var mobiles, dingtalkIds []string
+	isAtAll := client.ExtendDingtalk.IsAtAll
+	json.Unmarshal(client.ExtendDingtalk.AtMobiles, &mobiles)
+	json.Unmarshal(client.ExtendDingtalk.AtDingtalkIds, &dingtalkIds)
 
 	var msgList []any
 	//判断是否把消息聚合后发送
 	if isMerge {
 		msg := v1.MessageJoint(contentList, utils.VarDingtalk)
-		data := format.PackDingtalkMessage(client.ExtendDingtalk.RobotKeyword, msg, false, mobiles, dingtalkIds)
+		data := format.PackDingtalkMessage(client.ExtendDingtalk.RobotKeyword, msg, isAtAll, mobiles, dingtalkIds)
 		msgList = append(msgList, data)
 	} else {
 		for _, msg := range contentList {
-			data := format.PackDingtalkMessage(client.ExtendDingtalk.RobotKeyword, msg, true, mobiles, dingtalkIds)
+			data := format.PackDingtalkMessage(client.ExtendDingtalk.RobotKeyword, msg, isAtAll, mobiles, dingtalkIds)
 			msgList = append(msgList, data)
 		}
 	}
